ada9562ba659cb7c15be1cec6278cce9791842a1
Arter Tendean <arter@samb.co.id>
2024-11-25 13:09:48 +0700

    Adding quickstart code

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func handleGetRequest(c echo.Context) error {
+	return c.String(http.StatusOK, "arter tendean")
+}
+
+func handlePostRequest(c echo.Context) error {
+	return c.String(http.StatusOK, "arter")
+}
+
+func handlePutRequest(c echo.Context) error {
+	return c.String(http.StatusOK, "PUT request")
+}
+
+func handleDeleteRequest(c echo.Context) error {
+	return c.String(http.StatusOK, "DELETE request")
+}
 
 func main() {
 	fmt.Println("arter tendean")
+	e := echo.New()
+
+	e.GET("/", handleGetRequest)
+	e.POST("/", handlePostRequest)
+	e.PUT("/", handlePutRequest)
+	e.DELETE("/", handleDeleteRequest)
+
+	e.Logger.Fatal(e.Start(":8000"))
 }
