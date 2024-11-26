50edb002281107687d368a4e49ea97b6012b7a5a
Arter Tendean <arter@samb.co.id>
2024-11-26 09:59:36 +0700

    Adding Trailing Slash middleware

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"net/http"
 )
 
@@ -18,18 +19,42 @@ func handlePutRequest(c echo.Context) error {
 	return c.String(http.StatusOK, "PUT request")
 }
 
+type User struct {
+	Name    string
+	Address string
+	Age     int
+}
+
 func handleDeleteRequest(c echo.Context) error {
-	return c.String(http.StatusOK, "DELETE request")
+	//return c.String(http.StatusOK, "DELETE request")
+	user := User{"arter tendean", "jakarta", 123}
+	return c.JSON(http.StatusOK, user)
+}
+
+func getUser(c echo.Context) error {
+	id := c.Param("id")
+
+	fmt.Println("id:", id)
+
+	if id == "" {
+		return c.String(http.StatusOK, "`id` params is missing")
+	}
+
+	return c.String(http.StatusOK, "id params: "+id)
 }
 
 func main() {
 	fmt.Println("arter tendean")
 	e := echo.New()
 
+	e.Pre(middleware.RemoveTrailingSlash())
+
 	e.GET("/", handleGetRequest)
 	e.POST("/", handlePostRequest)
 	e.PUT("/", handlePutRequest)
 	e.DELETE("/", handleDeleteRequest)
+
+	e.GET("/users/:id", getUser)
 
 	e.Logger.Fatal(e.Start(":8000"))
 }
